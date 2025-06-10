2edc631140e831d9870adbaaead0719eaf61f37c
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-10 17:07:35 +0100

    production code

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@ let email = document.getElementById('email').value;
 let password = document.getElementById('password').value;
     try {
         
-        fetch('https://todos-list-app-production.onrender.com/registers', {
+        fetch('https://todos-list-app.onrender.com/registers', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
--- todos-list-app-main/backend/router/home.js
+++ todos-list-app-main/backend/router/home.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const path =  require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../../public', 'home.html'));
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 module.exports = router;
