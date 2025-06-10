d803a713c6d63e1b2a643c1be4e3018cdc697028
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-10 16:53:48 +0100

    Update home.js

--- todos-list-app-main/backend/router/home.js
+++ todos-list-app-main/backend/router/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-   res.sendFile('home.html')
+   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 module.exports = router;
