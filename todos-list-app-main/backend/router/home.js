006cb12ba4326c903ac1a41a9b993e0b3c04b94b
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-10 18:46:17 +0100

    Update home.js

--- todos-list-app-main/backend/router/home.js
+++ todos-list-app-main/backend/router/home.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'public', 'home.html'));
+   res.send('Welcome to akua backend');
 });
 
 module.exports = router;
