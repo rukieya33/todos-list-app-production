c8688723f57da2b0b6c84efa08f004cbe3969205
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-11 16:50:17 +0100

    Update home.js

--- todos-list-app-main/backend/router/home.js
+++ todos-list-app-main/backend/router/home.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
+
 router.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/', (req, res) => {
