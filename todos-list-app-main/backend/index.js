848c928742de958b61991f03f4b9d55ca9331e20
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-11 22:02:14 +0100

    Update index.js

--- todos-list-app-main/backend/index.js
+++ todos-list-app-main/backend/index.js
@@ -11,7 +11,7 @@ const port = 3000;
 // const cors = require('cors'); // CORS middleware to allow cross-origin requests    
 //app.options('*', cors()); // Enable pre-flight requests for all routes
 
-app.use(cors({origin:'https://sage-youtiao-e6765a.netlify.app/'}));
+app.use(cors({origin:'*'}));
  // Enable CORS for all origins // Middleware to parse URL-encoded bodies
 
 app.use('/', home);
