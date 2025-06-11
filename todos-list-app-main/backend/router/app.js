67b5dc833134e6e07783b81f33e9729193e779f3
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-11 17:05:27 +0100

    Update app.js

--- todos-list-app-main/backend/router/app.js
+++ todos-list-app-main/backend/router/app.js
@@ -1,4 +1,9 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
 router.use(express.static(path.join(__dirname, 'public')));
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
