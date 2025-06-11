479ba15f775e0d815c631578138112c6e27c83d7
Rukieya Ainoo <155789904+rukieya33@users.noreply.github.com>
2025-06-11 16:49:46 +0100

    Update home.js

--- todos-list-app-main/backend/router/home.js
+++ todos-list-app-main/backend/router/home.js
@@ -5,7 +5,7 @@ router.use(express.static(path.join(__dirname, 'public')));
 router.get('/', (req, res) => {
   res.send('Welcome to backend :)');
 });
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 module.exports = router;
